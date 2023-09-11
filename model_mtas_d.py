8445d2e8c6308879757de7f85f33e6ea8d3fe7df
gloriacxl <136415743+gloriacxl@users.noreply.github.com>
2023-09-11 16:23:19 +0800

    Add files via upload

---
+++ model_mtas_d.py
@@ -0,0 +1,353 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class ReconstructiveSubNetwork(nn.Module):
+    def __init__(self):
+        super(ReconstructiveSubNetwork, self).__init__()
+        # initialise network parameters
+        filter = [64, 128, 256, 512, 512]
+        # self.class_nb = 13
+
+        # define encoder decoder layers
+        self.encoder_block = nn.ModuleList([self.conv_layer([3, filter[0]])])
+        self.decoder_block = nn.ModuleList([self.conv_layer([filter[0], filter[0]])])
+        for i in range(4):
+            self.encoder_block.append(self.conv_layer([filter[i], filter[i + 1]]))
+            self.decoder_block.append(self.conv_layer([filter[i + 1], filter[i]]))
+
+        # define convolution layer
+        self.conv_block_enc = nn.ModuleList([self.conv_layer([filter[0], filter[0]])])
+        self.conv_block_dec = nn.ModuleList([self.conv_layer([filter[0], filter[0]])])
+        for i in range(4):
+            if i == 0:
+                self.conv_block_enc.append(self.conv_layer([filter[i + 1], filter[i + 1]]))
+                self.conv_block_dec.append(self.conv_layer([filter[i], filter[i]]))
+            else:
+                self.conv_block_enc.append(nn.Sequential(self.conv_layer([filter[i + 1], filter[i + 1]]),
+                                                         self.conv_layer([filter[i + 1], filter[i + 1]])))
+                self.conv_block_dec.append(nn.Sequential(self.conv_layer([filter[i], filter[i]]),
+                                                         self.conv_layer([filter[i], filter[i]])))
+
+        # define task attention layers
+        self.encoder_att = nn.ModuleList([nn.ModuleList([self.att_layer([filter[0], filter[0], filter[0]])])])
+        self.decoder_att = nn.ModuleList([nn.ModuleList([self.att_layer([2 * filter[0], filter[0], filter[0]])])])
+        self.encoder_block_att = nn.ModuleList([self.conv_layer([filter[0], filter[1]])])
+        self.decoder_block_att = nn.ModuleList([self.conv_layer([filter[0], filter[0]])])
+
+        for j in range(2):
+            if j < 1:
+                self.encoder_att.append(nn.ModuleList([self.att_layer([filter[0], filter[0], filter[0]])]))
+                self.decoder_att.append(nn.ModuleList([self.att_layer([2 * filter[0], filter[0], filter[0]])]))
+            for i in range(4):
+                self.encoder_att[j].append(self.att_layer([2 * filter[i + 1], filter[i + 1], filter[i + 1]]))
+                self.decoder_att[j].append(self.att_layer([filter[i + 1] + filter[i], filter[i], filter[i]]))
+
+        for i in range(4):
+            if i < 3:
+                self.encoder_block_att.append(self.conv_layer([filter[i + 1], filter[i + 2]]))
+                self.decoder_block_att.append(self.conv_layer([filter[i + 1], filter[i]]))
+            else:
+                self.encoder_block_att.append(self.conv_layer([filter[i + 1], filter[i + 1]]))
+                self.decoder_block_att.append(self.conv_layer([filter[i + 1], filter[i + 1]]))
+
+        self.pred_task1 = self.conv_layer([filter[0], 3], pred=True)
+        self.pred_task2 = self.conv_layer([filter[0], 2], pred=True)
+        # self.pred_task3 = self.conv_layer([filter[0], 2], pred=True)
+
+        # define pooling and unpooling functions
+        self.down_sampling = nn.MaxPool2d(kernel_size=2, stride=2, return_indices=True)
+        self.up_sampling = nn.MaxUnpool2d(kernel_size=2, stride=2)
+
+        self.logsigma = nn.Parameter(torch.FloatTensor([-0.5, -0.5, -0.5]))
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.xavier_normal_(m.weight)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.xavier_normal_(m.weight)
+                nn.init.constant_(m.bias, 0)
+
+    def conv_layer(self, channel, pred=False):
+        if not pred:
+            conv_block = nn.Sequential(
+                nn.Conv2d(in_channels=channel[0], out_channels=channel[1], kernel_size=3, padding=1),
+                nn.BatchNorm2d(num_features=channel[1]),
+                nn.ReLU(inplace=True),
+            )
+        else:
+            conv_block = nn.Sequential(
+                nn.Conv2d(in_channels=channel[0], out_channels=channel[0], kernel_size=3, padding=1),
+                nn.Conv2d(in_channels=channel[0], out_channels=channel[1], kernel_size=1, padding=0),
+            )
+        return conv_block
+
+    def att_layer(self, channel):
+        att_block = nn.Sequential(
+            nn.Conv2d(in_channels=channel[0], out_channels=channel[1], kernel_size=1, padding=0),
+            nn.BatchNorm2d(channel[1]),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=channel[1], out_channels=channel[2], kernel_size=1, padding=0),
+            nn.BatchNorm2d(channel[2]),
+            nn.Sigmoid(),
+        )
+        return att_block
+
+    def forward(self, x):
+        g_encoder, g_decoder, g_maxpool, g_upsampl, indices = ([0] * 5 for _ in range(5))
+        for i in range(5):
+            g_encoder[i], g_decoder[-i - 1] = ([0] * 2 for _ in range(2))
+
+        # define attention list for tasks
+        atten_encoder, atten_decoder = ([0] * 3 for _ in range(2))
+        for i in range(2):
+            atten_encoder[i], atten_decoder[i] = ([0] * 5 for _ in range(2))
+        for i in range(2):
+            for j in range(5):
+                atten_encoder[i][j], atten_decoder[i][j] = ([0] * 3 for _ in range(2))
+
+        # define global shared network
+        for i in range(5):
+            if i == 0:
+                g_encoder[i][0] = self.encoder_block[i](x)
+                g_encoder[i][1] = self.conv_block_enc[i](g_encoder[i][0])
+                g_maxpool[i], indices[i] = self.down_sampling(g_encoder[i][1])
+            else:
+                g_encoder[i][0] = self.encoder_block[i](g_maxpool[i - 1])
+                g_encoder[i][1] = self.conv_block_enc[i](g_encoder[i][0])
+                g_maxpool[i], indices[i] = self.down_sampling(g_encoder[i][1])
+
+        for i in range(5):
+            if i == 0:
+                g_upsampl[i] = self.up_sampling(g_maxpool[-1], indices[-i - 1])
+                g_decoder[i][0] = self.decoder_block[-i - 1](g_upsampl[i])
+                g_decoder[i][1] = self.conv_block_dec[-i - 1](g_decoder[i][0])
+            else:
+                g_upsampl[i] = self.up_sampling(g_decoder[i - 1][-1], indices[-i - 1])
+                g_decoder[i][0] = self.decoder_block[-i - 1](g_upsampl[i])
+                g_decoder[i][1] = self.conv_block_dec[-i - 1](g_decoder[i][0])
+
+        # define task dependent attention module
+        for i in range(2):
+            for j in range(5):
+                if j == 0:
+                    atten_encoder[i][j][0] = self.encoder_att[i][j](g_encoder[j][0])
+                    atten_encoder[i][j][1] = (atten_encoder[i][j][0]) * g_encoder[j][1]
+                    atten_encoder[i][j][2] = self.encoder_block_att[j](atten_encoder[i][j][1])
+                    atten_encoder[i][j][2] = F.max_pool2d(atten_encoder[i][j][2], kernel_size=2, stride=2)
+                else:
+                    atten_encoder[i][j][0] = self.encoder_att[i][j](torch.cat((g_encoder[j][0], atten_encoder[i][j - 1][2]), dim=1))
+                    atten_encoder[i][j][1] = (atten_encoder[i][j][0]) * g_encoder[j][1]
+                    atten_encoder[i][j][2] = self.encoder_block_att[j](atten_encoder[i][j][1])
+                    atten_encoder[i][j][2] = F.max_pool2d(atten_encoder[i][j][2], kernel_size=2, stride=2)
+
+            for j in range(5):
+                if j == 0:
+                    atten_decoder[i][j][0] = F.interpolate(atten_encoder[i][-1][-1], scale_factor=2, mode='bilinear', align_corners=True)
+                    atten_decoder[i][j][0] = self.decoder_block_att[-j - 1](atten_decoder[i][j][0])
+                    atten_decoder[i][j][1] = self.decoder_att[i][-j - 1](torch.cat((g_upsampl[j], atten_decoder[i][j][0]), dim=1))
+                    atten_decoder[i][j][2] = (atten_decoder[i][j][1]) * g_decoder[j][-1]
+                else:
+                    atten_decoder[i][j][0] = F.interpolate(atten_decoder[i][j - 1][2], scale_factor=2, mode='bilinear', align_corners=True)
+                    atten_decoder[i][j][0] = self.decoder_block_att[-j - 1](atten_decoder[i][j][0])
+                    atten_decoder[i][j][1] = self.decoder_att[i][-j - 1](torch.cat((g_upsampl[j], atten_decoder[i][j][0]), dim=1))
+                    atten_decoder[i][j][2] = (atten_decoder[i][j][1]) * g_decoder[j][-1]
+
+        # define task prediction layers
+        # t1_pred = F.log_softmax(self.pred_task1(atten_decoder[0][-1][-1]), dim=1)
+        t1_pred = self.pred_task1(atten_decoder[0][-1][-1])
+        t2_pred = self.pred_task2(atten_decoder[1][-1][-1])
+        # t3_pred = self.pred_task3(atten_decoder[2][-1][-1])
+        # t3_pred = t3_pred / torch.norm(t3_pred, p=2, dim=1, keepdim=True)
+
+        return t1_pred, t2_pred, self.logsigma
+
+
+
+
+class DiscriminativeSubNetwork(nn.Module):
+    def __init__(self,in_channels=3, out_channels=3, base_channels=64, out_features=False):
+        super(DiscriminativeSubNetwork, self).__init__()
+        base_width = base_channels
+        self.encoder_segment = EncoderDiscriminative(in_channels, base_width)
+        self.decoder_segment = DecoderDiscriminative(base_width, out_channels=out_channels)
+        #self.segment_act = torch.nn.Sigmoid()
+        self.out_features = out_features
+    def forward(self, x):
+        b1,b2,b3,b4,b5,b6 = self.encoder_segment(x)
+        output_segment = self.decoder_segment(b1,b2,b3,b4,b5,b6)
+        if self.out_features:
+            return output_segment, b2, b3, b4, b5, b6
+        else:
+            return output_segment
+
+class EncoderDiscriminative(nn.Module):
+    def __init__(self, in_channels, base_width):
+        super(EncoderDiscriminative, self).__init__()
+        self.block1 = nn.Sequential(
+            nn.Conv2d(in_channels,base_width, kernel_size=3, padding=1),
+            nn.BatchNorm2d(base_width),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(base_width, base_width, kernel_size=3, padding=1),
+            nn.BatchNorm2d(base_width),
+            nn.ReLU(inplace=True))
+        self.mp1 = nn.Sequential(nn.MaxPool2d(2))
+        self.block2 = nn.Sequential(
+            nn.Conv2d(base_width,base_width*2, kernel_size=3, padding=1),
+            nn.BatchNorm2d(base_width*2),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(base_width*2, base_width*2, kernel_size=3, padding=1),
+            nn.BatchNorm2d(base_width*2),
+            nn.ReLU(inplace=True))
+        self.mp2 = nn.Sequential(nn.MaxPool2d(2))
+        self.block3 = nn.Sequential(
+            nn.Conv2d(base_width*2,base_width*4, kernel_size=3, padding=1),
+            nn.BatchNorm2d(base_width*4),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(base_width*4, base_width*4, kernel_size=3, padding=1),
+            nn.BatchNorm2d(base_width*4),
+            nn.ReLU(inplace=True))
+        self.mp3 = nn.Sequential(nn.MaxPool2d(2))
+        self.block4 = nn.Sequential(
+            nn.Conv2d(base_width*4,base_width*8, kernel_size=3, padding=1),
+            nn.BatchNorm2d(base_width*8),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(base_width*8, base_width*8, kernel_size=3, padding=1),
+            nn.BatchNorm2d(base_width*8),
+            nn.ReLU(inplace=True))
+        self.mp4 = nn.Sequential(nn.MaxPool2d(2))
+        self.block5 = nn.Sequential(
+            nn.Conv2d(base_width*8,base_width*8, kernel_size=3, padding=1),
+            nn.BatchNorm2d(base_width*8),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(base_width*8, base_width*8, kernel_size=3, padding=1),
+            nn.BatchNorm2d(base_width*8),
+            nn.ReLU(inplace=True))
+
+        self.mp5 = nn.Sequential(nn.MaxPool2d(2))
+        self.block6 = nn.Sequential(
+            nn.Conv2d(base_width*8,base_width*8, kernel_size=3, padding=1),
+            nn.BatchNorm2d(base_width*8),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(base_width*8, base_width*8, kernel_size=3, padding=1),
+            nn.BatchNorm2d(base_width*8),
+            nn.ReLU(inplace=True))
+
+
+    def forward(self, x):
+        b1 = self.block1(x)
+        mp1 = self.mp1(b1)
+        b2 = self.block2(mp1)
+        mp2 = self.mp3(b2)
+        b3 = self.block3(mp2)
+        mp3 = self.mp3(b3)
+        b4 = self.block4(mp3)
+        mp4 = self.mp4(b4)
+        b5 = self.block5(mp4)
+        mp5 = self.mp5(b5)
+        b6 = self.block6(mp5)
+        return b1,b2,b3,b4,b5,b6
+
+class DecoderDiscriminative(nn.Module):
+    def __init__(self, base_width, out_channels=1):
+        super(DecoderDiscriminative, self).__init__()
+
+        self.up_b = nn.Sequential(nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True),
+                                 nn.Conv2d(base_width * 8, base_width * 8, kernel_size=3, padding=1),
+                                 nn.BatchNorm2d(base_width * 8),
+                                 nn.ReLU(inplace=True))
+        self.db_b = nn.Sequential(
+            nn.Conv2d(base_width*(8+8), base_width*8, kernel_size=3, padding=1),
+            nn.BatchNorm2d(base_width*8),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(base_width * 8, base_width * 8, kernel_size=3, padding=1),
+            nn.BatchNorm2d(base_width * 8),
+            nn.ReLU(inplace=True)
+        )
+
+
+        self.up1 = nn.Sequential(nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True),
+                                 nn.Conv2d(base_width * 8, base_width * 4, kernel_size=3, padding=1),
+                                 nn.BatchNorm2d(base_width * 4),
+                                 nn.ReLU(inplace=True))
+        self.db1 = nn.Sequential(
+            nn.Conv2d(base_width*(4+8), base_width*4, kernel_size=3, padding=1),
+            nn.BatchNorm2d(base_width*4),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(base_width * 4, base_width * 4, kernel_size=3, padding=1),
+            nn.BatchNorm2d(base_width * 4),
+            nn.ReLU(inplace=True)
+        )
+
+        self.up2 = nn.Sequential(nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True),
+                                 nn.Conv2d(base_width * 4, base_width * 2, kernel_size=3, padding=1),
+                                 nn.BatchNorm2d(base_width * 2),
+                                 nn.ReLU(inplace=True))
+        self.db2 = nn.Sequential(
+            nn.Conv2d(base_width*(2+4), base_width*2, kernel_size=3, padding=1),
+            nn.BatchNorm2d(base_width*2),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(base_width * 2, base_width * 2, kernel_size=3, padding=1),
+            nn.BatchNorm2d(base_width * 2),
+            nn.ReLU(inplace=True)
+        )
+
+        self.up3 = nn.Sequential(nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True),
+                                 nn.Conv2d(base_width * 2, base_width, kernel_size=3, padding=1),
+                                 nn.BatchNorm2d(base_width),
+                                 nn.ReLU(inplace=True))
+        self.db3 = nn.Sequential(
+            nn.Conv2d(base_width*(2+1), base_width, kernel_size=3, padding=1),
+            nn.BatchNorm2d(base_width),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(base_width, base_width, kernel_size=3, padding=1),
+            nn.BatchNorm2d(base_width),
+            nn.ReLU(inplace=True)
+        )
+
+        self.up4 = nn.Sequential(nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True),
+                                 nn.Conv2d(base_width, base_width, kernel_size=3, padding=1),
+                                 nn.BatchNorm2d(base_width),
+                                 nn.ReLU(inplace=True))
+        self.db4 = nn.Sequential(
+            nn.Conv2d(base_width*2, base_width, kernel_size=3, padding=1),
+            nn.BatchNorm2d(base_width),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(base_width, base_width, kernel_size=3, padding=1),
+            nn.BatchNorm2d(base_width),
+            nn.ReLU(inplace=True)
+        )
+
+
+
+        self.fin_out = nn.Sequential(nn.Conv2d(base_width, out_channels, kernel_size=3, padding=1))
+
+    def forward(self, b1,b2,b3,b4,b5,b6):
+        up_b = self.up_b(b6)
+        cat_b = torch.cat((up_b,b5),dim=1)
+        db_b = self.db_b(cat_b)
+
+        up1 = self.up1(db_b)
+        cat1 = torch.cat((up1,b4),dim=1)
+        db1 = self.db1(cat1)
+
+        up2 = self.up2(db1)
+        cat2 = torch.cat((up2,b3),dim=1)
+        db2 = self.db2(cat2)
+
+        up3 = self.up3(db2)
+        cat3 = torch.cat((up3,b2),dim=1)
+        db3 = self.db3(cat3)
+
+        up4 = self.up4(db3)
+        cat4 = torch.cat((up4,b1),dim=1)
+        db4 = self.db4(cat4)
+
+        out = self.fin_out(db4)
+        return out
+
